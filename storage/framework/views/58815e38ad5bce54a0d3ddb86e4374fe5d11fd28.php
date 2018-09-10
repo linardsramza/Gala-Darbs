9b345fedeced9f51594b0e8d9064f7ef9a17a823
linardsramza <41440541+linardsramza@users.noreply.github.com>
2018-09-11 01:02:45 +0300

    Add files via upload

---
+++ storage/framework/views/58815e38ad5bce54a0d3ddb86e4374fe5d11fd28.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <title>My project</title>
+
+    <!-- Scripts -->
+    <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+    <style>
+          html {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+        body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            background-image: url("/contactimg.jpg");
+            background-attachment: fixed;
+            background-size: 100% 100%;
+        }
+        #all {
+            min-height: 100%;
+            position: relative;
+        }
+        #footer {
+            height: 40px;
+            width: 100%;
+            position: absolute;
+            left: 0px;
+            bottom: 0px;
+        }
+        
+.futeris {
+    display: inline-block;
+    margin: 0 16px;
+    color: white;
+}
+    </style>
+</head>
+<body>
+<div id="all">
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark navbar-laravel">
+            <div class="container">
+                <a class="navbar-brand" href="<?php echo e(url('/')); ?>">
+                    Go to homepage
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php echo e(__('Toggle navigation')); ?>">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        <?php if(auth()->guard()->guest()): ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo e(route('login')); ?>"><?php echo e(__('Login')); ?></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo e(route('register')); ?>"><?php echo e(__('Register')); ?></a>
+                            </li>
+                        <?php else: ?>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <?php echo e(Auth::user()->name); ?> <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="<?php echo e(route('logout')); ?>"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        <?php echo e(__('Logout')); ?>
+
+                                    </a>
+
+                                    <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" style="display: none;">
+                                        <?php echo csrf_field(); ?>
+                                    </form>
+                                </div>
+                            </li>
+                        <?php endif; ?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            <?php echo $__env->yieldContent('content'); ?>
+        </main>
+    </div>
+    <div id="footer">
+      <footer style="background-color: black">
+            <nav>
+                <ul style="margin-bottom: 0px; padding-top: 10px">
+                    <li class="futeris" style="padding-left: 55px">
+                        <p>&copy; <?php echo @date("Y"); ?> Copyright</p>
+                    </li>
+                    <li class="futeris"  style="padding-left: 805px">
+                        <p>Developed by Linards Ramza</p>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
+        
+    </div>
+</div>
+</body>
+</html>
